d95ed953959713fcc88abe5c905eff41c1b16a48
kamilmatejuk <kamilmatejuk@gmail.com>
2021-02-17 15:04:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## About
+Program extracts the list of free proxys from [free-proxy-list.net](https://free-proxy-list.net/) and saves it into a specified file.
+
+### Running
+```
+python3 proxys.py <output-file>
+```
---
+++ proxys.py
@@ -0,0 +1,24 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def extract(file):
+    url = 'https://free-proxy-list.net/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    table = soup.select('[id=proxylisttable] tr')
+    ips = []
+    for row in table:
+        ip = ':'.join(t.text for t in row.select('td')[:2])
+        ips.append(ip)
+    print(f'Extracted {len(ips)} proxys from {url}')
+    with open(file, 'w+') as f:
+        f.writelines(ips)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Specify output file as a second argument')
+        exit(1)
+    extract(sys.argv[1])
